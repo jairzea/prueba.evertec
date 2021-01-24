46fc2fc3e7a0d699c5afd53edc2253c7e40988e6
Jair Zea <jairzeapaez@gmail.com>
2021-01-23 20:10:12 -0500

    Aplicacion de pruebas unitarias a Productos

---
+++ tests/Feature/ProductosTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductosTest extends TestCase
+{
+    /** @test */
+    public function testMostrarProductos()
+    {
+        $this->get('/productos')
+             ->assertStatus(200)
+             ->assertSee('detalles')
+             ->assertSee('name')
+             ->assertSee('description')
+             ->assertSee('price')
+             ->assertSee('sales')
+             ->assertSee('id');
+    }
+
+    /** @test */
+    function testRegistroProductos()
+    {
+        $this->post('/registro_productos', [
+            "nombre" => "Prueba de producto",
+            "descripcion"  => "Esto es un producto de prueba",
+            "precio" => "5000000",
+        ])
+        ->assertStatus(200)
+        ->assertSee('Producto registrado exitosamente');
+
+        $this->assertDatabaseHas('products', [
+            "name" => "Prueba de producto",
+            "description"  => "Esto es un producto de prueba",
+            "price" => "5000000",
+        ]);
+    }
+
+    /** @test */
+    function testActualizarProductos()
+    {
+        $this->put('/editar_producto/6', [
+            "nombre" => "Prueba editado",
+            "descripcion"  => "Este producto ha sido editado por una prueba unitaria",
+            "precio" => "5500000",
+        ])
+             ->assertStatus(200)
+             ->assertSee('Producto actualizado exitosamente');
+        
+        $this->assertDatabaseHas('products', [
+            "name" => "Prueba editado",
+            "description"  => "Este producto ha sido editado por una prueba unitaria",
+            "price" => "5500000",
+        ]);
+    }
+
+    /** @test */
+    function testBorrarProductos()
+    {
+        $this->delete('/borrar_producto/9')
+             ->assertStatus(200)
+             ->assertSee('Se ha borrado el producto con exito');
+    }
+
+}
